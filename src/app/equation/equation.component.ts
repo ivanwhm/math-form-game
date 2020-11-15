cb8e62cafb199ba30b7aec9c22572cfcd418da66
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-15 09:50:15 -0300

    Displaying Form Values

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get a(): number {
+    return this.mathForm.value.a;
+  }
+
+  get b(): number {
+    return this.mathForm.value.b;
+  }
+
+  private randomNumber(): number {
+    return Math.floor(Math.random() * 10);
+  }
 }
