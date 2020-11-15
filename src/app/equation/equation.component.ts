17f824f0b1a29976d6ddd0827b81d0238085fcc9
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-15 10:45:27 -0300

    Adding a Statistic

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -10,6 +10,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
   mathForm = new FormGroup(
     {
       a: new FormControl(this.randomNumber()),
@@ -19,15 +20,19 @@ export class EquationComponent implements OnInit {
     [MathValidators.addition('answer', 'a', 'b')]
   );
 
-  constructor() {}
-
   ngOnInit(): void {
+    const startTime = new Date();
+    let numberSolved = 0;
+
     this.mathForm.statusChanges
       .pipe(
         filter((value: string) => value === 'VALID'),
         delay(200)
       )
       .subscribe(() => {
+        numberSolved++;
+        this.secondsPerSolution =
+          (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
         this.mathForm.setValue({
           a: this.randomNumber(),
           b: this.randomNumber(),
